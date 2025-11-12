cb15a6b12732a855e53fdc8c22e0fefe8c53c176
GameBoyO2 <saubhagya0202@gmail.com>
2025-11-13 03:14:58 +0530

    Initial commit - added PDF merger script

---
+++ pdf.py
@@ -0,0 +1,15 @@
+from PyPDF2 import PdfWriter
+merger=PdfWriter()
+
+pdfs = []
+n=int(input("Enter number of PDF files to merge: "))
+
+for i in range(0,n):
+    name=input(f"Enter the file name{i+1}: ")
+    pdfs.append(name)
+    
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("merged-pdf.pdf")
+merger.close()
